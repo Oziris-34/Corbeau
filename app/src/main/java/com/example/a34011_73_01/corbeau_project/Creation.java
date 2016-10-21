611b6b5ae98fe93bb9e4308fb9b3aec760dd8313
Oziris-34 <o_ziris@hotmail.com>
2016-10-21 10:59:03 +0200

    creation save avant ajout anim

---
+++ app/src/main/java/com/example/a34011_73_01/corbeau_project/Creation.java
@@ -0,0 +1,17 @@
+package com.example.a34011_73_01.corbeau_project;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 34011-73-01 on 21/10/2016.
+ */
+
+public class Creation extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creation);
+    }
+}

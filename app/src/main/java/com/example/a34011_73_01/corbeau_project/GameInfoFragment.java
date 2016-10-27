6d6342866cdccb2ec0910c9434de96f9afa8c5e1
rougetaal <pasmod66@gmail.com>
2016-10-27 10:00:13 +0200

    Version test

---
+++ app/src/main/java/com/example/a34011_73_01/corbeau_project/GameInfoFragment.java
@@ -0,0 +1,56 @@
+package com.example.a34011_73_01.corbeau_project;
+
+import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * Created by 34011-73-09 on 27/10/2016.
+ */
+
+public class GameInfoFragment extends Fragment {
+    private OnOkPressedListener listener;
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        try {
+            listener = (OnOkPressedListener)activity;
+        }
+        catch(ClassCastException e) {
+            throw new ClassCastException(activity.toString() + "must implement OnOkPressedListener");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance) {
+        return inflater.inflate(R.layout.fragment_info, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstance) {
+        super.onActivityCreated(savedInstance);
+
+        Button ok = (Button)getView().findViewById(R.id.okButton);
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editText = (EditText)getView().findViewById(R.id.editText);
+                String text = editText.getText().toString();
+                if((text != null) && !text.trim().equals("")) {
+                    listener.onOkPressed(text);
+                }
+            }
+        });
+    }
+
+    public interface OnOkPressedListener {
+        public void onOkPressed(String name);
+    }
+}
--- app/src/main/java/com/example/a34011_73_01/corbeau_project/GameThread.java
+++ app/src/main/java/com/example/a34011_73_01/corbeau_project/GameThread.java
@@ -48,6 +48,7 @@ public class GameThread extends Thread {
 
     @Override
     public void run() {
+        /*
         long oldTime = SystemClock.elapsedRealtime();
         long newTime = 0;
         long changeTime = 0;
@@ -138,6 +139,6 @@ public class GameThread extends Thread {
                 }
             }
         }
-
+*/
     }
 }

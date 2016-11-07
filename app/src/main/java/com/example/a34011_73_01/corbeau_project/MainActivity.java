365531191021c7c01ce8b18e744ce3116e66d961
Oziris-34 <o_ziris@hotmail.com>
2016-11-07 08:43:20 +0100

    push pour resolution beug fin de parti

--- app/src/main/java/com/example/a34011_73_01/corbeau_project/MainActivity.java
+++ app/src/main/java/com/example/a34011_73_01/corbeau_project/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.a34011_73_01.corbeau_project;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Process;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,16 +10,22 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.music);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
 
         Button newGameButton = (Button)findViewById(R.id.newGame);
         newGameButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                mediaPlayer.stop();
                 startActivity(intent);
             }
         });
@@ -28,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, Credit.class);
+                mediaPlayer.stop();
                 startActivity(intent);
             }
         });
@@ -36,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         quitGameButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                mediaPlayer.stop();
                 Process.killProcess(Process.myPid());
             }
         });
